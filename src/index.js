96ee58cd173e53fce15400d6e047805f7077f36d
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-08 10:27:18 -0400

    instructions placed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // write your code here
+
+// {
+//     "id": 1,
+//     "name": "Shoyu Ramen",
+//     "restaurant": "Nonono",
+//     "image": "./assets/ramen/shoyu.jpg",
+//     "rating": 7,
+//     "comment": "Delish. Can't go wrong with a classic!"
+//   },
+
+// - See all ramen images in the `div` with the id of `ramen-menu`. 
+//When the page loads, request the data from the server to get all the ramen objects. Then,
+//   display the image for each of the ramen using an `img` tag inside the
+//   `#ramen-menu` div.
+
+
+
+// - Click on an image from the `#ramen-menu` div and see all the info about that
+//   ramen displayed inside the `#ramen-detail` div and where it says
+//   `insert comment here` and `insert rating here`.
+
+
+
+// - Create a new ramen after submitting the `new-ramen` form. The new ramen should
+//   be added to the`#ramen-menu` div. The new ramen does not need to persist; in
+//   other words, if you refresh the page, it's okay that the new ramen is no
+//   longer on the page.
